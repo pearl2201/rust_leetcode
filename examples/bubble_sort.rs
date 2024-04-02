ff91d6f2c4bfd4b8796f8d2d6ad2624d0c3ff45f
nguyenanhngoc <ngoc.nguyenanh@inwave.vn>
2024-04-02 17:49:12 +0700

    Add bubble sort

---
+++ examples/bubble_sort.rs
@@ -0,0 +1,26 @@
+use std::fs;
+fn main() {
+    let input = fs::read_to_string("./assets/bubble_sort.txt").unwrap();
+    let mut arr: Vec<u32> = input
+        .split_ascii_whitespace()
+        .map(|x| {
+            return x.parse::<u32>().unwrap();
+        })
+        .collect();
+
+    let mut temp: u32;
+    let n = arr.len();
+    for i in 0..(n) {
+        for j in ((i + 1)..(n)).rev() {
+            if arr[j] < arr[j - 1] {
+                temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    println!(
+        "{}",
+        arr.into_iter().map(|x| x.to_string()).collect::<Vec<_>>().join(",")
+    );
+}
